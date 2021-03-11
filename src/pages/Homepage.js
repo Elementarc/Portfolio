0aecd1aad9342f219fa106faf52238976d5723fa
VoidTale <aVoidTale@gmail.com>
2021-03-11 16:35:29 +0100

    update to interface Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react"
+//needs icon as Prop
 import SelectIcon from "../components/SelectIcon"
 import Particles from "react-particles-js"
 import particlesConfig from "../assets/ParticlesConfig/particlesjs-config.json"
+import '../assets/css/homepage.css';
 //Import component
 import Nav from "../components/Nav"
 //Animation for navigation
@@ -44,15 +46,58 @@ const HomepageContainer = (props) => {
     return(
         <div className="homepageContainer" id="home">
             <div className="homePageBackground">
-                <Particles params={particlesConfig}></Particles>
+                    <Particles params={particlesConfig}></Particles>
             </div>
             
             <div className="homepageContentContainer">
                 <div className="topSectionContainer">
                     <div className="homepageTopDecoration"></div>
-                    <div onClick={toggleNav} className="openNavIconContainer"><SelectIcon icon="MenuIcon"/></div>
+                    <div className="moonContainer">
+                        <div className="moonLight"> </div>
+                        <div className="moonLightV2"> </div>
+                    <div/>
                 </div>
-                    <div className="middleSectionContainer"></div>
+                    
+                <div onClick={toggleNav} className="openNavIconContainer"><SelectIcon icon="MenuIcon"/></div>
+                </div>
+                <div className="middleSectionContainer">
+                    <h1>{"DESIGN & MORE"}</h1>
+                    <h2>THE WORLD OF DESIGN</h2>
+                    <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
+                    <button>GET STARTED</button>
+                </div>
+                <div className="sectionContainer">
+                    
+                    <div class="sections sectionsTarget">
+                        <div className="sectionShadowTarget"></div>
+                    </div>
+
+                    <span />
+
+                    <div class="sections">
+                        <div></div>
+                    </div>
+
+                    <span />
+
+                    <div class="sections">
+                        <div></div>
+                    </div>
+
+                    <span />
+
+                    <div class="sections">
+                        <div></div>
+                    </div>
+                </div>
+
+                <div className="bottomSectionContainer">
+                    <div className="slideShow">
+                        <div className="slide1"></div>
+                        <div className="slide2"></div>
+                        <div className="slide3"></div>
+                    </div>
+                </div>
                 <Nav toggleNav={toggleNav}/>
             </div>
                         
