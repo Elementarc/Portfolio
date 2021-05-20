a2263ee89dd0b24e4a7f3f9c59d5f8b35ff79406
VoidTale <aVoidTale@gmail.com>
2021-05-20 16:17:43 +0200

    Finished sectionManager overhaul final hover effects were added and now looks awesome

--- src/components/HomepageComponents/SectionManager.tsx
+++ src/components/HomepageComponents/SectionManager.tsx
@@ -11,12 +11,22 @@ const SectionManager = (props: any) => {
     const location = useLocation()
     const history = useHistory()
 
+    function hoverEffects(hover: boolean, linkIndex: number){
+        if(window.innerWidth > 1000){
+            var getIcon = document.querySelectorAll(".icon")
+            var getSectionName = document.querySelectorAll(".sectionName")
 
-    //cleanUp 
-    useEffect(() => {
-        
-        
-    }, []);
+            if(hover === true){
+                getIcon[linkIndex].classList.add("svgIconHover")
+                getSectionName[linkIndex].classList.add("sectionNameHover")
+            }
+            else{
+                getSectionName[linkIndex].classList.remove("sectionNameHover")
+                getIcon[linkIndex].classList.remove("svgIconHover")
+            }
+        }
+    }
+
     //Saving props into cache to not always re compute it on every rerender
     var properties = useMemo(() =>{
         var properties = {
@@ -73,10 +83,9 @@ const SectionManager = (props: any) => {
         })
     }, [properties,history.location.pathname])
 
+    //Setting target Styles for sectionManager on Url change
     useEffect(() => {
-        
         function setTarget() {
-            console.log(history.location.pathname)
             var getIcon = document.querySelectorAll(".icon")
             var getIconShadow = document.querySelectorAll(".iconShadow")
             var getSectionName = document.querySelectorAll(".sectionName")
@@ -85,97 +94,113 @@ const SectionManager = (props: any) => {
                 getIcon[0].classList.add("svgIconTarget")
                 getIconShadow[0].classList.add("iconShadowTarget")
                 getSectionName[0].classList.add("sectionNameTarget")
+                //New Class for sectionName To only add opacity 1 because media query for mobile is: p{opacity: 0}
+                getSectionName[0].classList.add("pTarget")
 
                 getIcon[1].classList.remove("svgIconTarget")
                 getIconShadow[1].classList.remove("iconShadowTarget")
                 getSectionName[1].classList.remove("sectionNameTarget")
+                getSectionName[1].classList.remove("pTarget")
 
                 getIcon[2].classList.remove("svgIconTarget")
                 getIconShadow[2].classList.remove("iconShadowTarget")
                 getSectionName[2].classList.remove("sectionNameTarget")
+                getSectionName[2].classList.remove("pTarget")
 
                 getIcon[3].classList.remove("svgIconTarget")
                 getIconShadow[3].classList.remove("iconShadowTarget")
                 getSectionName[3].classList.remove("sectionNameTarget")
+                getSectionName[3].classList.remove("pTarget")
             }
             else if(location.pathname.toLowerCase() === "/home/strength"){
 
                 getIcon[1].classList.add("svgIconTarget")
                 getIconShadow[1].classList.add("iconShadowTarget")
                 getSectionName[1].classList.add("sectionNameTarget")
+                getSectionName[1].classList.add("pTarget")
 
                 getIcon[0].classList.remove("svgIconTarget")
                 getIconShadow[0].classList.remove("iconShadowTarget")
                 getSectionName[0].classList.remove("sectionNameTarget")
+                getSectionName[0].classList.remove("pTarget")
 
                 getIcon[2].classList.remove("svgIconTarget")
                 getIconShadow[2].classList.remove("iconShadowTarget")
                 getSectionName[2].classList.remove("sectionNameTarget")
+                getSectionName[2].classList.remove("pTarget")
 
                 getIcon[3].classList.remove("svgIconTarget")
                 getIconShadow[3].classList.remove("iconShadowTarget")
                 getSectionName[3].classList.remove("sectionNameTarget")
+                getSectionName[3].classList.remove("pTarget")
             }
             else if(location.pathname.toLowerCase() === "/home/routine"){
                 getIcon[2].classList.add("svgIconTarget")
                 getIconShadow[2].classList.add("iconShadowTarget")
                 getSectionName[2].classList.add("sectionNameTarget")
+                getSectionName[2].classList.add("pTarget")
 
                 getIcon[1].classList.remove("svgIconTarget")
                 getIconShadow[1].classList.remove("iconShadowTarget")
                 getSectionName[1].classList.remove("sectionNameTarget")
+                getSectionName[1].classList.remove("pTarget")
 
                 getIcon[0].classList.remove("svgIconTarget")
                 getIconShadow[0].classList.remove("iconShadowTarget")
                 getSectionName[0].classList.remove("sectionNameTarget")
+                getSectionName[0].classList.remove("pTarget")
 
                 getIcon[3].classList.remove("svgIconTarget")
                 getIconShadow[3].classList.remove("iconShadowTarget")
                 getSectionName[3].classList.remove("sectionNameTarget")
+                getSectionName[3].classList.remove("pTarget")
             }
             else if(location.pathname.toLowerCase() === "/home/daily"){
                 getIcon[3].classList.add("svgIconTarget")
                 getIconShadow[3].classList.add("iconShadowTarget")
                 getSectionName[3].classList.add("sectionNameTarget")
+                getSectionName[3].classList.add("pTarget")
 
                 getIcon[1].classList.remove("svgIconTarget")
                 getIconShadow[1].classList.remove("iconShadowTarget")
                 getSectionName[1].classList.remove("sectionNameTarget")
+                getSectionName[1].classList.remove("pTarget")
 
                 getIcon[2].classList.remove("svgIconTarget")
                 getIconShadow[2].classList.remove("iconShadowTarget")
                 getSectionName[2].classList.remove("sectionNameTarget")
+                getSectionName[2].classList.remove("pTarget")
 
                 getIcon[0].classList.remove("svgIconTarget")
                 getIconShadow[0].classList.remove("iconShadowTarget")
                 getSectionName[0].classList.remove("sectionNameTarget")
+                getSectionName[0].classList.remove("pTarget")
             }
         }
-        
         setTarget()
     }, [location.pathname]);
     return (
         <motion.div  animate="in" exit="out" initial="initial" variants={props.interfaceAnimation} className="SectionContainer">
-            <Link onClick={() => properties.setLocationIndex(0)} to="/home">
+            <Link onMouseEnter={() => hoverEffects(true, 0)} onMouseLeave={() => hoverEffects(false, 0)} onClick={() => properties.setLocationIndex(0)} to="/home">
                 <div className="iconShadow" />
                 <p  className="sectionName" id="sectionName0">Homepage</p>
 
                 <HomePagination className="icon" id="icon0" />
             </Link>
             <span />
-            <Link onClick={() => properties.setLocationIndex(1)}  to="/home/strength">
+            <Link onMouseEnter={() => hoverEffects(true, 1)} onMouseLeave={() => hoverEffects(false, 1)} onClick={() => properties.setLocationIndex(1)}  to="/home/strength">
                 <div className="iconShadow" />
                 <p className="sectionName" id="sectionName1">Strength</p>
                 <HomePagination  className="icon" id="icon1"/>
             </Link>
             <span />
-            <Link onClick={() => properties.setLocationIndex(2)} to="/home/routine">
+            <Link onMouseEnter={() => hoverEffects(true, 2)} onMouseLeave={() => hoverEffects(false, 2)} onClick={() => properties.setLocationIndex(2)} to="/home/routine">
                 <div className="iconShadow" />
                 <p className="sectionName" id="sectionName2">Routine</p>
                 <HomePagination className="icon" id="icon2"/>
             </Link>
             <span />
-            <Link onClick={() => properties.setLocationIndex(3)} to="/home/daily">
+            <Link onMouseEnter={() => hoverEffects(true, 3)} onMouseLeave={() => hoverEffects(false, 3)} onClick={() => properties.setLocationIndex(3)} to="/home/daily">
                 <div className="iconShadow" />
                 <p className="sectionName" id="sectionName3">Daily</p>
                 <HomePagination className="icon" id="icon3"/>
